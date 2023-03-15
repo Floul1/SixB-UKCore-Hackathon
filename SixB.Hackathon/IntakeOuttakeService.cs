489317c7e8d94f2d2d13d41d8482e0bde1d6845f
Owen Hamer <owen.hamer@6bdigital.com>
2023-03-15 10:39:38 +0000

    Fix Discharge

--- SixB.Hackathon/IntakeOuttakeService.cs
+++ SixB.Hackathon/IntakeOuttakeService.cs
@@ -1,5 +1,6 @@
 ﻿using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
 using Newtonsoft.Json;
 using Task = System.Threading.Tasks.Task;
 
@@ -9,13 +10,15 @@ public class IntakeOuttakeService
 {
     private ResourceReference _personalWardTeam =
         new ResourceReference($"CareTeam/1add538f-ae39-4176-88ec-e37b35b9a6fa", "6B Virtual Ward Team");
-    
-    private ResourceReference _managingOrg = new ResourceReference("Organization/0920c700-f51e-40cc-a297-dfff43801dd5", "North West Ambulance Trust");
+
+    private ResourceReference _managingOrg = new ResourceReference("Organization/0920c700-f51e-40cc-a297-dfff43801dd5",
+        "North West Ambulance Trust");
+
     private string _managingOdsCode = "RX7";
 
-    private ResourceReference _doctorRef = new ResourceReference($"1bdd198b-4ce1-4aa8-a1ee-12c922d30a6c")
+    private ResourceReference _doctorRef = new ResourceReference($"Practitioner/1bdd198b-4ce1-4aa8-a1ee-12c922d30a6c")
     {
-        Identifier = new Identifier("https://fhir.hl7.org.uk/Id/gmp-number","G0109459")
+        Identifier = new Identifier("https://fhir.hl7.org.uk/Id/gmp-number", "G0109459")
     };
 
     private FhirClient _client;
@@ -24,7 +27,7 @@ public class IntakeOuttakeService
     {
         _client = new FhirClient("https://3cdzg7kbj4.execute-api.eu-west-2.amazonaws.com/poc/events/FHIR/R4/");
     }
-    
+
     public async Task<string> AdmitPatientToVirtualWard(string nhsNumber)
     {
         var patient = new ResourceReference()
@@ -32,14 +35,17 @@ public class IntakeOuttakeService
             Identifier = new Identifier("https://fhir.nhs.uk/Id/nhs-number", nhsNumber)
         };
         var start = GenerateStartOfEpisode(patient, "RX7"); //Hardcoded for POC
-        var admit = GenerateAdmitEncounter(_doctorRef, patient, new ResourceReference($"urn:uuid:{start.Id}"), _personalWardTeam);
+        var admit = GenerateAdmitEncounter(_doctorRef, patient, new ResourceReference($"urn:uuid:{start.Id}"),
+            _personalWardTeam);
 
         var transactionBundle = new Bundle
         {
+            Id = Guid.NewGuid().ToString(),
             Type = Bundle.BundleType.Transaction
         };
         transactionBundle.Entry.Add(new Bundle.EntryComponent
         {
+            FullUrlElement = new FhirUri($"urn:uuid:{start.Id}"),
             Resource = start,
             Request = new Bundle.RequestComponent
             {
@@ -49,6 +55,7 @@ public class IntakeOuttakeService
         });
         transactionBundle.Entry.Add(new Bundle.EntryComponent
         {
+            FullUrlElement = new FhirUri($"urn:uuid:{admit.Id}"),
             Resource = admit,
             Request = new Bundle.RequestComponent
             {
@@ -56,13 +63,25 @@ public class IntakeOuttakeService
                 Method = Bundle.HTTPVerb.POST
             }
         });
-        var res = await _client.TransactionAsync(transactionBundle);
-        if(res == null) throw new NullReferenceException("Failed to create episode of care");
-        Console.WriteLine(JsonConvert.SerializeObject(res));
-        var episodeOfCare = res.Entry.FirstOrDefault(x => x.Resource is EpisodeOfCare)?.Resource as EpisodeOfCare;
-        if (episodeOfCare == null) throw new NullReferenceException("Failed to create episode of care");
-        return episodeOfCare.Id;
+        var fhirJson = new FhirJsonSerializer();
+        var encode = fhirJson.SerializeToString(transactionBundle);
+        try
+        {
+            var res = await _client.TransactionAsync(transactionBundle);
+            if (res == null) throw new NullReferenceException("Failed to create episode of care");
+            Console.WriteLine(JsonConvert.SerializeObject(res));
+            var episodeOfCare = res.Entry
+                .FirstOrDefault(x => x.Response != null && x.Response.Location.Contains("EpisodeOfCare"))?.Response;
+            if (episodeOfCare == null || !episodeOfCare.Status.Contains("201")) throw new NullReferenceException("Failed to create episode of care");
+            return start.Id;
+        }
+        catch (FhirOperationException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
+
     public async Task DischargePatient(string nhsNumber, string episodeOfCareIdentifier)
     {
         var patientIdentifier = new ResourceReference
@@ -77,11 +96,13 @@ public class IntakeOuttakeService
         {
             Type = Bundle.BundleType.Transaction
         };
-        var endOfEpisode = await GetAndEndEpisodeOfCare(episodeOfCareIdentifier);
+        var endOfEpisode = await GetAndEndEpisodeOfCare(nhsNumber);
         if (endOfEpisode == null) throw new NullReferenceException("Failed to find end of episode");
-        var discharge = GenerateDischargeEncounter(_doctorRef, patientIdentifier, new ResourceReference($"urn:uuid:{endOfEpisode.Id}"), _personalWardTeam);
+        var discharge = GenerateDischargeEncounter(_doctorRef, patientIdentifier,
+            new ResourceReference($"urn:uuid:{endOfEpisode.Id}"), _personalWardTeam);
         transactionBundle.Entry.Add(new Bundle.EntryComponent
         {
+            FullUrlElement = new FhirUri($"urn:uuid:{endOfEpisode.Id}"),
             Resource = endOfEpisode,
             Request = new Bundle.RequestComponent
             {
@@ -91,6 +112,7 @@ public class IntakeOuttakeService
         });
         transactionBundle.Entry.Add(new Bundle.EntryComponent
         {
+            FullUrlElement = new FhirUri($"urn:uuid:{discharge.Id}"),
             Resource = discharge,
             Request = new Bundle.RequestComponent
             {
@@ -98,6 +120,18 @@ public class IntakeOuttakeService
                 Method = Bundle.HTTPVerb.POST
             }
         });
+        try
+        {
+            var result = await _client.TransactionAsync(transactionBundle);
+            Console.WriteLine("Patient has been discharged");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
+        
     }
 
     /// <summary>
@@ -110,6 +144,7 @@ public class IntakeOuttakeService
     {
         var episodeOfCare = new EpisodeOfCare
         {
+            Id = Guid.NewGuid().ToString(),
             Status = EpisodeOfCare.EpisodeOfCareStatus.Active,
             Patient = patientReference,
             ManagingOrganization = new ResourceReference
@@ -140,9 +175,9 @@ public class IntakeOuttakeService
         return episodeOfCare;
     }
 
-    private async Task<EpisodeOfCare?> GetAndEndEpisodeOfCare(string episodeOfCareIdentifier)
+    private async Task<EpisodeOfCare?> GetAndEndEpisodeOfCare(string patientIdentifier)
     {
-        var eoc = await _client.GetAsync($"EpisodeOfCare/{episodeOfCareIdentifier}");
+        var eoc = await _client.GetAsync($"EpisodeOfCare?patient:identifier=https://fhir.nhs.uk/Id/nhs-number|{patientIdentifier}&status=active");
         if (eoc == null || eoc is not Bundle bundle) return null;
         if (!bundle.Any()) return null;
         var episodeOfCare = bundle.Entry.First().Resource as EpisodeOfCare;
--- SixB.Hackathon/Program.cs
+++ SixB.Hackathon/Program.cs
@@ -6,7 +6,7 @@ using SixB.Hackathon;
 Console.WriteLine("Hello, World!");
 
 var service = new ObservationService();
-await service.CreateObservation("RX7", "456", "789", "9234234599", 1.2m);
+// await service.CreateObservation("RX7", "456", "789", "9234234599", 1.2m);
 var newService = new IntakeOuttakeService();
-await newService.AdmitPatientToVirtualWard("9234234599");
-await newService.DischargePatient("9234234599", "RX7");
+var eocId = await newService.AdmitPatientToVirtualWard("9234234599");
+await newService.DischargePatient("9234234599", eocId);
